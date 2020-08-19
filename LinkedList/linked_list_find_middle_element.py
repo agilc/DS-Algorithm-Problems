55f74d02c8231405e9cd186b01d90dd256c8ae7b
agilc <agilc315@gmail.com>
2020-08-19 21:52:49 +0530

    find middle element of an linked list by traversing two times

---
+++ LinkedList/linked_list_find_middle_element.py
@@ -0,0 +1,39 @@
+class Node():
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+class LinkedList():
+  def __init__(self):
+    self.head = None
+
+	#Inserting at the begining of a linkedlist
+  def push(self, new_data):
+    new_node = Node(new_data)
+    new_node.next = self.head
+    self.head = new_node
+  
+  #printing the linked list
+  def print_list(self):
+    temp = self.head
+    while temp:
+      print(f'{temp.data} -->')
+      temp = temp.next
+
+  #Find the middle element in a linked list
+  def find_middle_element_traversing(self):
+    if self.head is None:
+      print("Linked list is empty")
+      return
+
+    temp = self.head
+    count = 0
+    while temp:
+      temp = temp.next
+      count += 1
+
+    temp = self.head
+    for _ in range(count//2):
+      temp = temp.next
+
+    print(f'The middle element of the linked list is {temp.data}')
